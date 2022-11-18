a675cc3c66602e9cb549508af0d06238530f66c1
deeptikalegore <deeptikalegore@gmail.com>
2022-11-18 21:50:35 +0530

    initial

---
+++ JewelleryManagement/JewelleryManagement/Model/JewelleryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Numerics;
+
+namespace JewelleryManagement.Model
+{
+    public class JewelleryContext : DbContext
+    {
+        public JewelleryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Jewellery> Jewelleries { get; set; }
+    }
+}
